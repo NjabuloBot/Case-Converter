3643d97b479ddf64fb4237ecd4b348da61a0d86b
Njabulo <nnshiwu@gmail.com>
2024-02-17 10:11:54 +0200

    Added '_'+char.lower() if char.isupper() inside the list.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
    # snake_cased_string = ''.join(snake_cased_char_list)
    # clean_snake_cased_string = snake_cased_string.strip('_')
    # return clean_snake_cased_string 
-   snake_cased_char_list = []
+   snake_cased_char_list = ['_'+char.lower() if char.isupper()]
 
 def main():
    print(convert_to_snake_case('aLongAndComplexString'))
