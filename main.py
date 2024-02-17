604d5e31994fe8009bc023918e941c12c1537dc6
Njabulo <nnshiwu@gmail.com>
2024-02-17 10:16:41 +0200

    Added return ''.join(snake_cased_char_list).

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
       else char
       for char in pascal_or_camel_cased_string
       ]
+   
+   return ''.join(snake_cased_char_list)
 
 def main():
    print(convert_to_snake_case('aLongAndComplexString'))
